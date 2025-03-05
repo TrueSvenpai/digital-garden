f53e324f33653670874fe33b168808e4c6750828
Sven Reinelt <sven.reinelt@gmail.com>
2025-03-05 11:35:23 +0100

    Quartz sync: Mar 5, 2025, 11:35 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 TrueSvenpai0",
+    pageTitle: "🪴 TrueSvenpa0",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
